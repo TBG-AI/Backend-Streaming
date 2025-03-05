18de7498cc042ec1aa60750bff40e60619b4a833
RyanK0902 <txryank@stanford.edu>
2025-03-04 23:03:13 -0800

    merged in streaming-robust

---
+++ src/backend_streaming/providers/whoscored/infra/repos/file_repo.py
@@ -0,0 +1,83 @@
+from pathlib import Path
+import json
+import logging
+from typing import Dict, Optional
+from backend_streaming.providers.whoscored.infra.config.config import PathConfig, TypeToPaths
+
+# TODO: change this to a database!
+
+class FileRepository:
+    """
+    File-based repository for storing and retrieving internal json files.
+    NOTE: all mappings are included as json files for now
+    """
+    def __init__(
+            self, 
+            paths: PathConfig, 
+            type_to_paths: TypeToPaths, 
+            logger: logging.Logger
+        ):
+        self.paths = paths
+        self.type_to_paths = type_to_paths
+        self.logger = logger
+        
+    def _get_path(self, file_type: str, file_name: Optional[str] = None) -> Path:
+        """Get the correct path based on file type"""
+        if file_type == 'player':
+            return self.type_to_paths.PLAYER
+        elif file_type == 'player_addition':
+            return self.type_to_paths.PLAYER_ADDITION
+        elif file_type == 'team':
+            return self.type_to_paths.TEAM
+        elif file_type == 'match':
+            return self.type_to_paths.MATCH
+        
+        # NOTE: these directories require file name since they are game specific
+        elif file_name:
+            if file_type == 'raw_pagesources':
+                return self.type_to_paths.RAW_PAGESOURCES / file_name
+            elif file_type == 'lineups':
+                return self.type_to_paths.LINEUPS / file_name
+            elif file_type == 'parsed_page_sources':
+                return self.type_to_paths.PARSED_PAGE_SOURCES / file_name
+        else:
+            raise ValueError(f"Unknown file type: {file_type}")
+
+    def load(
+        self, 
+        file_type: str, 
+        is_txt: bool = False, 
+        file_name: Optional[str] = None
+    ) -> Dict[str, str]:
+        """
+        Load mappings based on mapping type
+        """
+        try:
+            file_path = self._get_path(file_type, file_name)  
+            if is_txt:
+                return file_path.read_text()
+            else:
+                with open(file_path) as f:
+                    return json.load(f)
+                
+        except FileNotFoundError as e:
+            self.logger.warning(f"File not found: {e.filename}. Creating new.")
+            return {}
+            
+    def save(
+        self, 
+        file_type: str, 
+        data: Dict[str, str], 
+        file_name: Optional[str] = None
+    ) -> None:
+        """
+        Save mappings based on mapping type
+        """
+        try:
+            file_path = self._get_path(file_type, file_name)
+            with open(file_path, 'w') as f:
+                json.dump(data, f, indent=2)
+                
+        except Exception as e:
+            self.logger.error(f"Failed to save mappings: {e}")
+            raise
